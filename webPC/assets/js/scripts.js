46f59bde0d05dff80dfe6df7d55abd66d1f91bf3
limmiehoang <minhkhanhhoang2105@gmail.com>
2018-12-24 10:33:48 +0700

    refactor(DropdownMenu): remove unused code, refactor scripts.js

--- webPC/assets/js/scripts.js
+++ webPC/assets/js/scripts.js
@@ -1,23 +1,21 @@
-$(".menu_icon").click(function() {
+var $window = $(window);
+
+function checkWidth() {
+    var windowsize = $window.width();
+    if (windowsize > 768) {
+        $(".navigation").show();
+    }
+    else {
+        $(".navigation").hide();
+    }
+}
+// Execute on load
+checkWidth();
+// Bind event listener
+$(window).resize(checkWidth);
+
+// Show or hide navigation list when menu is clicked
+$(".menu_icon").on('click', function() {
     $(".navigation")
         .toggle();
-});
-$(document).ready(function() {
-    // Optimalisation: Store the references outside the event handler:
-    var $window = $(window);
-
-    function checkWidth() {
-        var windowsize = $window.width();
-        if (windowsize > 768) {
-            $(".navigation").show();
-        }
-        else {
-            $(".navigation").hide();
-            console.log("hide");
-        }
-    }
-    // Execute on load
-    checkWidth();
-    // Bind event listener
-    $(window).resize(checkWidth);
 });
